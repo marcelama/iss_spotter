a911d5d6f68253424f738c477b0e69ef66279d84
Marcela Ang <marcela.ang@gmail.com>
2022-11-18 22:19:23 -0500

    M2W5 - ISS promises

--- index.js
+++ index.js
@@ -6,16 +6,8 @@
 
 
 const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation  } = require('./iss');
+const { printPassTimes } = require('./printPassTimes');
 
-
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
